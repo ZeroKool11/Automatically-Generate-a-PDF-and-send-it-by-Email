79b01c5a1c950d7f05c8f2fee2eae86aac94d8a0
Dany Silva <perseo.11@gmail.com>
2020-09-30 02:48:51 +0000

    Building functions ChangePermissions and CopyFile

--- initialize.py
+++ initialize.py
@@ -1,9 +1,33 @@
 #!/usr/bin/env python3
 
+import subprocess
 import shutil as sh
 import os
 
-destPath = os.path.realpath('../scripts/example.py')
-print(destPath)
-sh.copy("example.py", + destPath)
-#print(result)
+def ChangePermissions():
+# Cambiar permisos de example.py
+	result = subprocess.run(['chmod','o+wx','example.py'])
+	if result.returncode != 0:
+		print("No se lograron cambiar los permisos de el archivo example.py. " + str(result))
+		return False
+	return True
+
+def CopyFile():
+# Copiar example.py a la ruta correcta
+	destPath = os.path.realpath('../scripts/example.py')
+	try:
+		sh.copy("example.py", destPath)
+		return(True)
+	except Exception as e:
+		print("No se logro copiar el archivo example.py. " + str(e))
+		return(False)
+
+
+def main():
+	if not ChangePermissions(): exit()
+	if not CopyFile(): exit()
+	
+
+
+if __name__ == "__main__":
+	main()
